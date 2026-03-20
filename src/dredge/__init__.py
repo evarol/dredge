d2ca9b2632e8d87c98529f904fc60bc6f70427af
Charlie Windolf <charliexwindolf@gmail.com>
2026-03-20 13:16:15 -0400

    fix api

---
+++ src/dredge/__init__.py
@@ -0,0 +1,7 @@
+from . import dredge_ap, dredge_lfp, dredgelib, motion_util
+from .motion_util import get_motion_estimate
+from .motion_util import plot_me_traces
+from .motion_util import speed_limit_filter
+from .dredge_ap import register
+from .dredge_lfp import register_online_lfp
+from .dredgelib import thomas_solve, normxcorr1d
